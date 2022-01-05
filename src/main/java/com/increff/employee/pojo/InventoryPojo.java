b64e378802fed5786d4d94de036b6dec24451033
kishanhitk <kishansharma1231@gmail.com>
2022-01-05 20:37:24 +0530

    feat: add inventory Pojo

--- src/main/java/com/increff/employee/pojo/InventoryPojo.java
+++ src/main/java/com/increff/employee/pojo/InventoryPojo.java
@@ -1,18 +1,30 @@
 package com.increff.employee.pojo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity()
 @Table(name = "Inventory")
 public class InventoryPojo {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private int quantity;
+    private int productId;
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
     }
 
     public int getId() {
