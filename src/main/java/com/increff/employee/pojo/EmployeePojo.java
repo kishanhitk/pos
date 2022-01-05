07e5c1ea54af8e65df5ace75c952ecbfacaaf259
kishanhitk <kishansharma1231@gmail.com>
2022-01-05 19:53:23 +0530

    feat: add @table name in existing pojo

--- src/main/java/com/increff/employee/pojo/EmployeePojo.java
+++ src/main/java/com/increff/employee/pojo/EmployeePojo.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity()
+@Table(name = "Employee")
 public class EmployeePojo {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private int age;
