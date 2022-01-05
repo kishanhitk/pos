ed71097d3a5c88bb5fe0f27480926c1abc791597
kishanhitk <kishansharma1231@gmail.com>
2022-01-05 19:52:54 +0530

    feat: add brand category table

---
+++ src/main/java/com/increff/employee/pojo/BrandCategoryPojo.java
@@ -0,0 +1,34 @@
+package com.increff.employee.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity()
+@Table(name = "BrandCategory")
+public class BrandCategoryPojo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String brand;
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
