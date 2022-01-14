2e9c1c17130730525af5e3e376fab0c4bf87b2ba
kishanhitk <kishansharma1231@gmail.com>
2022-01-14 20:52:26 +0530

    feat: add delete button on dynamic row

--- src/main/webapp/static/order.js
+++ src/main/webapp/static/order.js
@@ -1,23 +1,25 @@
 function addRow() {
   // grab form
-  const id = Math.random() * 1000;
+  const id = Math.floor(Math.random() * 10000000);
   $("#order-form-div")
-    .append(`<div class="form-row d-flex justify-content-between">
+    .append(`<div class="form-row d-flex justify-content-between" id="row-${id}">
   <div class="form-group col-4">
   <label for="inputBarcode${id}">Barcode</label>
-  <input type="text" class="form-control" id="inputBarcode${id}"
+  <input type="text" class="form-control" required id="inputBarcode${id}"
   placeholder="Enter Product Barcode">
   </div>
-  <div class="form-group col-4">
+  <div class="form-group col-3">
   <label for="inputQuantity${id}">Barcode</label>
-  <input type="number" class="form-control" id="inputQuantity${id}" placeholder="Quantity">
+  <input type="number" class="form-control" required id="inputQuantity${id}" placeholder="Quantity">
   </div>
-  <div class="form-group col-4">
+  <div class="form-group col-3">
   <label for="inputSellingPrice${id}">Barcode</label>
-  <input type="number" step="0.01" class="form-control" id="inputSellingPrice${id}"
+  <input type="number" step="0.01" class="form-control"  required id="inputSellingPrice${id}"
   placeholder="Selling Price">
   </div>
+  <button class="btn btn-danger delete-row col-1" type="button" id=${id}>X</button>
   </div>`);
+  $(".delete-row").on("click", deleteRow);
 }
 
 function placeOrder(e) {
@@ -32,9 +34,16 @@ function placeOrder(e) {
   console.log(order);
 }
 
+function deleteRow() {
+  var id = $(this).attr("id");
+  console.log(id);
+  $("#row-" + id).remove();
+}
+
 function init() {
   $("#add-row").click(addRow);
   $("#order-form").submit(placeOrder);
+  $(".delete-row").on("click", deleteRow);
 }
 
 $(document).ready(init);
