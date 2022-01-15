6d49ff8147dc9a93b93725eb04b862508a8dd0e4
kishanhitk <kishansharma1231@gmail.com>
2022-01-15 18:26:20 +0530

    feat: add filelength check

--- src/main/webapp/static/app.js
+++ src/main/webapp/static/app.js
@@ -1,54 +1,56 @@
-
 //HELPER METHOD
-function toJson($form){
-    var serialized = $form.serializeArray();
-    console.log(serialized);
-    var s = '';
-    var data = {};
-    for(s in serialized){
-        data[serialized[s]['name']] = serialized[s]['value']
-    }
-    var json = JSON.stringify(data);
-    return json;
+function toJson($form) {
+  var serialized = $form.serializeArray();
+  console.log(serialized);
+  var s = "";
+  var data = {};
+  for (s in serialized) {
+    data[serialized[s]["name"]] = serialized[s]["value"];
+  }
+  var json = JSON.stringify(data);
+  return json;
 }
 
-
-function handleAjaxError(response){
-	var response = JSON.parse(response.responseText);
-	alert(response.message);
+function handleAjaxError(response) {
+  var response = JSON.parse(response.responseText);
+  alert(response.message);
 }
 
-function readFileData(file, callback){
-	var config = {
-		header: true,
-		delimiter: "\t",
-		skipEmptyLines: "greedy",
-		complete: function(results) {
-			callback(results);
-	  	}	
-	}
-	Papa.parse(file, config);
+function readFileData(file, callback) {
+  var config = {
+    header: true,
+    delimiter: "\t",
+    skipEmptyLines: "greedy",
+    complete: function (results) {
+      //Check that file length should not be greater than 5000
+      if (results.data.length > 5000) {
+        alert("File size exceeds 5000 rows");
+        return;
+      }
+      callback(results);
+    },
+  };
+  Papa.parse(file, config);
 }
 
+function writeFileData(arr) {
+  var config = {
+    quoteChar: "",
+    escapeChar: "",
+    delimiter: "\t",
+  };
 
-function writeFileData(arr){
-	var config = {
-		quoteChar: '',
-		escapeChar: '',
-		delimiter: "\t"
-	};
-	
-	var data = Papa.unparse(arr, config);
-    var blob = new Blob([data], {type: 'text/tsv;charset=utf-8;'});
-    var fileUrl =  null;
+  var data = Papa.unparse(arr, config);
+  var blob = new Blob([data], { type: "text/tsv;charset=utf-8;" });
+  var fileUrl = null;
 
-    if (navigator.msSaveBlob) {
-        fileUrl = navigator.msSaveBlob(blob, 'download.tsv');
-    } else {
-        fileUrl = window.URL.createObjectURL(blob);
-    }
-    var tempLink = document.createElement('a');
-    tempLink.href = fileUrl;
-    tempLink.setAttribute('download', 'download.tsv');
-    tempLink.click(); 
+  if (navigator.msSaveBlob) {
+    fileUrl = navigator.msSaveBlob(blob, "download.tsv");
+  } else {
+    fileUrl = window.URL.createObjectURL(blob);
+  }
+  var tempLink = document.createElement("a");
+  tempLink.href = fileUrl;
+  tempLink.setAttribute("download", "download.tsv");
+  tempLink.click();
 }
