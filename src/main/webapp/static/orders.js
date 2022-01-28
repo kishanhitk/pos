563ee48ad0e4dabc9abc58e19f27173ae997067a
kishanhitk <kishansharma1231@gmail.com>
2022-01-28 10:41:00 +0530

    feat: remove edit order option from orders

--- src/main/webapp/static/orders.js
+++ src/main/webapp/static/orders.js
@@ -79,9 +79,11 @@ function displayOrderList(data) {
     var buttonHtml =
       ' <button class="btn btn-primary" onclick="displayOrderDetails(' +
       e.id +
-      ')">Details</button> <button class="btn btn-outline-primary ml-3" onclick="displayEditOrderForm(' +
-      e.id +
-      ')">Edit</button> ';
+      ')">Details</button>';
+    // +
+    // '<button class="btn btn-outline-primary ml-3" onclick="displayEditOrderForm(' +
+    // e.id +
+    // ')">Edit</button> ';
     var row =
       "<tr>" +
       "<td>" +
